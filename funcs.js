99204209d9bf5ab186c1b042971a6f65a30aad1b
EinanCO <92308515+EinanCO@users.noreply.github.com>
2023-04-04 19:01:59 +0300

    Add files via upload

--- funcs.js
+++ funcs.js
@@ -1,3 +1,15 @@
-function checkLogin(){
-    window.alert("dddd"); 
+function checkLogin() {
+    var uname = document.getElementById("username").value;
+    if (!uname.includes("@"))
+        window.alert("incorrect email address");
+    var pass = document.getElementById("password").value;
+    // if (pass == "")
+    //     window.alert("Fill password field please");
+    // if (pass.length < 6)
+    //     window.alert("Password length must be atleast 6 characters");
+    var decimal = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{6,40}$/;
+    if (pass.match(decimal))
+        alert("good");
+    else
+        alert("bad");
 }
