8be824a5f30d3fb7669f0e5a0a5da77b578fcff2
EinanCO <92308515+EinanCO@users.noreply.github.com>
2023-04-05 19:09:56 +0300

    ..

--- funcs.js
+++ funcs.js
@@ -7,8 +7,13 @@ function checkLogin() {
     //     window.alert("Fill password field please");
     // if (pass.length < 6)
     //     window.alert("Password length must be atleast 6 characters");
-    var decimal = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{6,40}$/;
-    if (pass.match(decimal))
+    //var decimal = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{6,40}$/;
+
+    var lowerCaseLetters = /[a-z]/g;
+    var upperCaseLetters= /[A-Z]/g;
+    //var specialLetters= /[]g;
+    var numbers = /[0-9]/g;
+    if (pass.match(lowerCaseLetters))
         alert("good");
     else
         alert("bad");
