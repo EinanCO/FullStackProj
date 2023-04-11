fc8bde48c28b11604b39347f56e77e4c469a1562
EinanCO <92308515+EinanCO@users.noreply.github.com>
2023-04-11 11:30:37 +0300

    Update funcs.js

--- funcs.js
+++ funcs.js
@@ -1,20 +1,29 @@
 function checkLogin() {
+    let errors=[];
     var uname = document.getElementById("username").value;
-    if (!uname.includes("@"))
-        window.alert("incorrect email address");
     var pass = document.getElementById("password").value;
-    // if (pass == "")
-    //     window.alert("Fill password field please");
-    // if (pass.length < 6)
-    //     window.alert("Password length must be atleast 6 characters");
-    //var decimal = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{6,40}$/;
-
     var lowerCaseLetters = /[a-z]/g;
     var upperCaseLetters= /[A-Z]/g;
-    //var specialLetters= /[]g;
-    var numbers = /[0-9]/g;
-    if (pass.match(lowerCaseLetters))
-        alert("good");
-    else
-        alert("bad");
+    var digit = /[0-9]/g;
+    var specialLetters= /[!@#$%^&*()+=_-]/g;
+
+    if (!uname.includes("@"))
+        errors.push("incorrect email address");
+    if(pass=="")
+        errors.push("Fill password field");   
+    if (!pass.match(lowerCaseLetters))
+        errors.push("Must include at least one lowercase letter");
+    if (!pass.match(upperCaseLetters))
+        errors.push("Must include at least one uppercase letter");
+    if(!pass.match(digit))
+        errors.push("Must include at least one digit");
+    if(pass.length<6)
+        errors.push("Password length must be atleast 6 characters");
+    if (!pass.match(specialLetters))
+        errors.push("Must include at least one special character");
+
+    errors.forEach((err) => alert(err));
+    if(errors.length==0)
+    alert(uname+"\n"+pass);   
 }
+    //var decimal = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{6,40}$/;
